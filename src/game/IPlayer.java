a8fc7cb0bfcb9e064313b1096d6366d1f4560289
mat285 <mat285@cornell.edu>
2016-10-13 15:39:03 -0400

    Player interface

--- src/game/IPlayer.java
+++ src/game/IPlayer.java
@@ -1,4 +1,10 @@
 package game;
 
+import card.*;
+
 public interface IPlayer {
+    public void Initialize();
+    public void StartRound(Card[] hand);
+    public CardPassMove PassCards();
+    public Move Play(GameState state);
 }
