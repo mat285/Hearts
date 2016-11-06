164acd93137dac68678a2d14f299f64eb36c640b
mat285 <mat285@cornell.edu>
2016-11-06 01:57:26 -0400

    Added terminal cardpassing

--- src/player/TerminalPlayer.java
+++ src/player/TerminalPlayer.java
@@ -8,14 +8,25 @@ public class TerminalPlayer extends AbstractPlayer implements IPlayer {
 
     @Override
     public CardPassMove PassCards(SealedGameInfo info) {
-        return GameUtils.RandomCardPass(info);
+        System.out.println("Hand: " + getHand(info));
+        System.out.println("Which Cards?");
+        Scanner s = new Scanner(System.in);
+        Card c1=null, c2=null, c3=null;
+        int tries = 3;
+        do {
+            tries--;
+            String[] cards = s.nextLine().split(", ");
+            if (cards.length != 3) continue;
+            c1 = Cards.Parse(cards[0]);
+            c2 = Cards.Parse(cards[1]);
+            c3 = Cards.Parse(cards[2]);
+        } while ((c1 == null || c2 == null || c3 == null) && tries > 0);
+        return new CardPassMove(c1,c2,c3);
     }
 
     @Override
     public Move Play(SealedGameInfo info) {
-        List<Card> hand = new ArrayList<>(info.GetHand());
-        Collections.sort(hand);
-        System.out.println("Hand: " + hand);
+        System.out.println("Hand: " + getHand(info));
         System.out.println("Trick: " + info.CurrentTrick().AllCards());
         System.out.println("Which Card?");
         Scanner s = new Scanner(System.in);
@@ -26,5 +37,11 @@ public class TerminalPlayer extends AbstractPlayer implements IPlayer {
             tries--;
         } while (c == null && tries > 0);
         return new Move(c);
+    }
+
+    private List<Card> getHand(SealedGameInfo info) {
+        List<Card> hand = new ArrayList<>(info.GetHand());
+        Collections.sort(hand);
+        return hand;
     }
 }
