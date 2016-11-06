08437ae3bee683a278a12f9f8b70bd73028cb358
mat285 <mat285@cornell.edu>
2016-11-06 00:11:21 -0400

    oops

---
+++ src/game/Move.java
@@ -0,0 +1,23 @@
+package game;
+
+import card.Card;
+
+public class Move {
+    private Card _card;
+
+    public Move(Card card) {
+        this._card = card;
+    }
+
+    public Card Card() {
+        return _card;
+    }
+
+    public boolean equals(Object o) {
+        return (o instanceof  Move) && ((Move) o).Card().equals(Card());
+    }
+
+    public int hashCode() {
+        return Card().hashCode();
+    }
+}
